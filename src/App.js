99508f35dd5a7644a852b273e623949906b66f12
Ilya Savchenko <97258310+st1gmat@users.noreply.github.com>
2023-10-19 17:39:37 +0300

    added mui-icons, changed index.html

--- src/App.js
+++ src/App.js
@@ -1,26 +1,9 @@
 import './App.css';
-import Button from '@mui/material/Button';
-import Container from '@mui/material/Container';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
 
 function App() {
   return (
     <div className="App">
-      <Container>
-      <Box
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        <Typography variant="body1" gutterBottom>
-          Starred Page
-        </Typography>
-      </Box>
-    </Container>
+      
     </div>
   );
 }
