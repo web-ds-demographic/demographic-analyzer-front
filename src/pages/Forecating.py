12898b6f8605827614c61e762dabb072f88f7f7b
Ilya Savchenko <97258310+st1gmat@users.noreply.github.com>
2023-11-28 21:23:59 +0300

    added choose-input for params to draw on linechart

--- src/pages/Forecating.py
+++ src/pages/Forecating.py
@@ -3,7 +3,6 @@ import pandas as pd
 import plotly.express as px
 from test_data.alg import get_pred
 
-            
 # Стилизация страницы
 st.markdown("""
     <style>
@@ -17,12 +16,10 @@ st.markdown("""
     </style>
 """, unsafe_allow_html=True)
 
-
 st.title("Прогнозирование демографических данных")
 
 countries = ["Senegal", "Russia"]
 selected_country = st.selectbox("Выберите страну из доступных:", countries)
-
 
 if selected_country == "Senegal":
     file_path = "./test_data/senegal.csv"
@@ -31,14 +28,37 @@ else:
 
 df = pd.read_csv(file_path)
 
+
+
 years_to_forecast = st.slider("Выберите количество лет для прогнозирования:", 1, 30, 5)
 
 # Получение прогноза
 result = get_pred(df, years_to_forecast)
 
+# Выбор отображаемых столбцов
+selected_columns = st.multiselect("Выберите столбцы для отображения:", result.columns)
+
+# Информация о столбцах
+column_info = {
+    'N(t)': 'Количество населения',
+    'B(t)': 'Рождаемость',
+    'NM(t)': 'Количество миграций',
+    'D(t)': 'Количество смертей',
+    'DNM': 'Смерти + миграции',
+    'IntB': 'Сумма рождаемости за все время',
+    'IntDNM': 'Сумма (Смерти + миграции) за все время',
+    'rBt': 'Процентный прирост рождаемости',
+    'rDNMt': 'Процентный прирост (Смерти + миграции)',
+    'Qt': 'Показатель изменчивости'
+}
+
+# Отображение информации о столбцах
+for col in selected_columns:
+    st.write(f"**{col}**: {column_info.get(col, 'Нет информации')}")
+
+
 st.subheader("Результат прогнозирования:")
 st.write(result.set_index('Year'))
-fig = px.line(result, x='Year', y=result.columns[1:], title='Прогноз демографических данных')
+fig = px.line(result, x='Year', y=selected_columns, title='Прогноз демографических данных')
 fig.update_layout(width=900, height=500, xaxis_title='Год', yaxis_title='Значение')
 st.plotly_chart(fig)
-# st.linechart(result.set_index('Year'), width=900, height=500, use_container_width=True)
